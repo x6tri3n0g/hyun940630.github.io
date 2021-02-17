4a5be6fe845e6eadcb9a551bb9fb4c0c78523741
xtring <hh940630@gmail.com>
2021-02-17 23:56:25 +0900

    Add: Footer

---
+++ src/components/Home/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FooterWrapper } from './Atoms';
+
+const Footer = () => {
+    return (
+        <FooterWrapper>
+            <ul>
+                <li>Developed by Hyun(xtring)</li>
+                <li>Designed by Yong(ruddy)</li>
+                <li>Contact me by email or sns.</li>
+            </ul>
+        </FooterWrapper>
+    );
+};
+
+export default Footer;
