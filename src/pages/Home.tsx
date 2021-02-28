28c86a379792ad12252df6c4bc6726bacdf0b72e
xtring <hh940630@gmail.com>
2021-02-28 23:32:30 +0900

    Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ interface HomeProps {
     mode: string;
 }
 
-const Home: React.FC<HomeProps> = ({ mode = 'light' }): React.ReactElement => {
+const Home: React.FC<HomeProps> = ({ mode }): React.ReactElement => {
     return (
         <PageLayout mode={mode}>
             <TopNavigation />
