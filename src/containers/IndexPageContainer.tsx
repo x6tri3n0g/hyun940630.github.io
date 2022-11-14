e84a6a3066947b4703bc97cccdfb22a8fae0f9f8
xtring <hh940630@gmail.com>
2022-11-14 23:50:20 +0900

    remove portfolio

--- src/containers/IndexPageContainer.tsx
+++ src/containers/IndexPageContainer.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import {MainContentsWrapper} from 'components/Home/MainContents/Atoms';
+import { MainContentsWrapper } from 'components/Home/MainContents/Atoms';
 import TopNavigation from 'components/TopNavigation';
 import Header from 'components/Home/Header';
 import WhoAmI from 'components/Home/MainContents/WhoAmI';
 import Skills from 'components/Home/MainContents/Skills';
 import Experience from 'components/Home/MainContents/Experience';
-// import Portfolio from 'components/Home/MainContents/Portfolio';
 import Interest from 'components/Home/MainContents/Interest';
 import Footer from 'components/Home/Footer';
 import styled from '@emotion/styled';
@@ -20,18 +18,17 @@ const Container = styled.div`
 `;
 
 export default function PageContainer() {
-    return (
-        <Container>
-            <TopNavigation/>
-            <Header/>
-            <MainContentsWrapper>
-                <WhoAmI/>
-                <Skills/>
-                <Experience/>
-                {/* <Portfolio /> */}
-                <Interest/>
-                <Footer/>
-            </MainContentsWrapper>
-        </Container>
-    );
+  return (
+    <Container>
+      <TopNavigation />
+      <Header />
+      <MainContentsWrapper>
+        <WhoAmI />
+        <Skills />
+        <Experience />
+        <Interest />
+        <Footer />
+      </MainContentsWrapper>
+    </Container>
+  );
 }
